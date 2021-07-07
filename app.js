61934452cf03a62167ea1a1cc9202b3f9dd2d156
qinpengcheng <754530433@qq.com>
2021-07-07 17:06:33 +0800

    fix:修改每日打卡题目样式

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ App({
     // 新增school_id
     globalData: {
         organizationId:3,// 机构id
-        version:685,// 版本号
+        version:370,// 版本号
         userInfo: null,
         rdsession: "",
         acct_id: 1,
