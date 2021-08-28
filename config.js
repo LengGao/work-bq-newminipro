b2d532c689b2f850a6af05eccfcf0e6920372441
qinpengcheng <754530433@qq.com>
2021-08-28 18:05:28 +0800

    fix:修改题目展示

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 let config = {
   imgUrl: 'http://oss.beiqujy.com/applet/',
-  // bqUrl: 'https://dpapi.beiqujy.com/wechat/v2',//正式地址
+  bqUrl: 'https://dpapi.beiqujy.com/wechat/v2',//正式地址
   // bqUrl: 'https://stand.api.beiqujy.com/wechat/v2',//正式地址
   liaotian: 'wss://dpapi.beiqujy.com/chat', //正式地址
   // bqUrl: 'https://apps.beiqujy.com/'
   //bqUrl: 'http://dongpei.local/wechat/v2',//
-   bqUrl: 'https://testapi.beiqujy.com/wechat/v2',//测试地址
+  //  bqUrl: 'https://testapi.beiqujy.com/wechat/v2',//测试地址
   //  bqUrl: 'https://formal.api.beiqujy.com/wechat/v2',
   // bqUrl: 'http://edu.com/wechat/v2',
   // bqUrl: 'http://sc.dp.com/wechat/v2',
