d707da7604871bb9e7ec593a12b85cc55547defd
qinpengcheng <754530433@qq.com>
2021-12-21 17:47:27 +0800

    sl

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ App({
     // 新增school_id
     globalData: {
         organizationId: 3,// 机构id
-        version: 798,// 版本号
+        version: 28,// 版本号
         userInfo: null,
         rdsession: "",
         acct_id: 1,
